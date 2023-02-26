a54c5085641d3a87e748cde50e79d615eee035d6
Goodarzyar <goodarzyar@ifb.ir>
2023-02-26 14:04:31 +0330

    session50: build explanation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import styles from '@/styles/Home.module.css'
 
-export default function Home({posts}) {
+export default function Home({posts}:any) {
 
   console.log(posts);
   return (
@@ -8,26 +8,27 @@ export default function Home({posts}) {
    <h3>Botostart Next.js Course</h3>
    <ul>
     {
-      posts.map((post)=>(<li key={ post.id }>{ post.title }</li>))
+      posts.map((post:any)=>(<li key={ post.id }>{ post.title }</li>))
     }
    </ul>
-   {/* <h1>{data} you can do it</h1>  */}
   </div>
   )
 }
 
 export async function getStaticProps() {
-  //fetch data from api
 
-  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-  const data = await res.json();
-  
- 
-  console.log(data);
+  // fetch data from api
+   const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+   const data = await res.json();
+  // console.log(data);
+
+  // const res = await fetch("http://localhost:3001/posts");
+  // const data = await res.json();
 
   return {
     props:{
-      posts:data,
+      posts: data,
     }
   };
+
 }
