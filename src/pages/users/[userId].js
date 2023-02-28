cd503257c1b34d548fe2cf47aed9956f7eb0b7ff
Goodarzyar <goodarzyar@ifb.ir>
2023-02-28 12:17:51 +0330

    session54: getStaticPaths : create dynamic route

--- src/pages/users/[userId].js
+++ src/pages/users/[userId].js
@@ -1,16 +1,52 @@
-function UserDetail() {
+function UserDetail({data}) {
     return ( 
     <div>
         UserDetail
+        <h1>
+{data.name}
+        </h1>
+        <h3>
+            {data.email}
+        </h3>
     </div> 
     );
 }
 
 export default UserDetail;
+
+export async function getStaticPaths(){
+
+    const res = await fetch(`https://jsonplaceholder.typicode.com/users`);
+    const data = await res.json();
+    const paths = data.map(user=>({ params: {userId: user.id.toString()} } ));
+
+    return{
+
+        // paths:[
+        //     {params:{userId:"1"}},
+        //     {params:{userId:"2"}},
+        //     {params:{userId:"3"}},
+        //     {params:{userId:"4"}},
+        //     {params:{userId:"5"}},
+        //     {params:{userId:"6"}},
+        //     {params:{userId:"7"}},
+        //     {params:{userId:"8"}},
+        //     {params:{userId:"9"}},
+        //     {params:{userId:"10"}},
+        // ],
+
+        //paths : paths,
+
+        paths,
+        fallback:false
+    }
+}
+
+
 export async function getStaticProps(context){
 
     const {params} = context;
-    const res = await fetch(`https://jsonplaceholder.typicode.com/users/${Params.userId}`);
+    const res = await fetch(`https://jsonplaceholder.typicode.com/users/${params.userId}`);
     const data = await res.json();
 
     console.log(params)
