0f0ddc909f40550912b58b92894d39cb1bbd2311
Goodarzyar <goodarzyar@ifb.ir>
2023-03-13 16:28:30 +0330

    session75:SWR React Hooks for Data Fetching

---
+++ src/pages/todos-SWR/index.js
@@ -0,0 +1,18 @@
+import useSWR from 'swr';
+
+function SWRTodos() {
+   // const [todos,setTodos]= useState([]);
+
+    const {data,error}= useSWR("http://localhost:3001/todos",(url)=>fetch(url).then((res)=>res.json()));
+
+    console.log({data,error})
+    return ( <div>
+    {data ? data.map(t=><h3 key={t.id}>{t.title}</h3>) 
+    :<h1>Loading...</h1>}
+    </div> );
+
+
+
+}
+
+export default SWRTodos;
